f620b4af3cc1dc4dbf8b6daaa7ea100ab098947c
dloyolar <dieegolr@gmail.com>
2022-03-14 19:31:18 -0300

    [ADD] ✨ Handle context for open/close sidebar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -10,13 +10,17 @@ import {
 } from '@mui/material';
 
 import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import { useContext } from 'react';
 import MailOutLineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import { UIContext } from '../../context/ui';
 
 const menuItems: string[] = ['Inbox', 'Starred', 'Send email', 'Drafts'];
 
 export const Sidebar = () => {
+  const { sidemenuOpen, closeSideMenu } = useContext(UIContext);
+
   return (
-    <Drawer anchor="left" open={false} onClose={() => console.log('closing')}>
+    <Drawer anchor="left" open={sidemenuOpen} onClose={closeSideMenu}>
       <Box sx={{ width: 250 }}>
         <Box sx={{ padding: '5px 10px' }}>
           <Typography variant="h4">Menu</Typography>
--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -12,10 +12,16 @@ const UI_INITIAL_STATE: UIState = {
 export const UIProvider: FC = ({ children }) => {
   const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
 
+  const openSideMenu = () => dispatch({ type: 'UI - Open Sidebar' });
+
+  const closeSideMenu = () => dispatch({ type: 'UI - Close Sidebar' });
+
   return (
     <UIContext.Provider
       value={{
-        sidemenuOpen: false,
+        ...state,
+        openSideMenu,
+        closeSideMenu,
       }}
     >
       {children}
