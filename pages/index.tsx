7a1e1010fc43277a5c7cb4860f1af1eddc42c59c
dloyolar <dieegolr@gmail.com>
2022-03-17 22:43:28 -0300

    [ADD] ✨Entries context, reducer and provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
+
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant="h1" color="primary">
-        Hello World
-      </Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Pending" />
+            <CardContent>
+              {/* Add new entry */}
+              {/* Entries list */}
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px )' }}>
+            <CardHeader title="Finish" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
