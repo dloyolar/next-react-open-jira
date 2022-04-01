e93c672ffc6aa0236273c61563cd61ddb998bed9
dloyolar <dieegolr@gmail.com>
2022-04-01 12:45:34 -0300

    [ADD] ✨ edit entry page with form

---
+++ pages/entries/[id].tsx
@@ -0,0 +1,103 @@
+import { useState, ChangeEvent } from 'react';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import {
+  capitalize,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  TextField,
+  CardActions,
+  Button,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from '@mui/material';
+import { Layout } from '../../components/layouts';
+import { EntryStatus } from '../../interfaces/entry';
+import { IconButton } from '@mui/material';
+
+const validStatus: EntryStatus[] = ['pending', 'in-progress', 'finished'];
+
+export const EntryPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [status, setStatus] = useState<EntryStatus>('pending');
+  const [touched, setTouched] = useState(false);
+
+  const onInputValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const onStatusChanged = (e: ChangeEvent<HTMLInputElement>) => {
+    setStatus(e.target.value as EntryStatus);
+  };
+
+  const onSave = () => {
+    console.log({ inputValue, status });
+  };
+
+  return (
+    <Layout>
+      <Grid container justifyContent="center" sx={{ marginTop: 2 }}>
+        <Grid item xs={12} sm={8} md={6}>
+          <Card>
+            <CardHeader
+              title={`Entry ${inputValue}`}
+              subheader={'Create 4mins ago'}
+            />
+            <CardContent>
+              <TextField
+                sx={{ marginTop: 2, marginBottom: 1 }}
+                fullWidth
+                placeholder="New entry"
+                autoFocus
+                multiline
+                label="New entry"
+                value={inputValue}
+                onChange={onInputValueChange}
+              />
+              <FormControl>
+                <FormLabel>Status:</FormLabel>
+                <RadioGroup row value={status} onChange={onStatusChanged}>
+                  {validStatus.map((status) => (
+                    <FormControlLabel
+                      key={status}
+                      value={status}
+                      control={<Radio />}
+                      label={capitalize(status)}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </CardContent>
+            <CardActions>
+              <Button
+                startIcon={<SaveOutlinedIcon />}
+                variant="contained"
+                fullWidth
+                onClick={onSave}
+              >
+                Save
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      <IconButton
+        sx={{
+          position: 'fixed',
+          bottom: 30,
+          right: 30,
+          backgroundColor: 'error.dark',
+        }}
+      >
+        <DeleteOutlinedIcon />
+      </IconButton>
+    </Layout>
+  );
+};
+
+export default EntryPage;
